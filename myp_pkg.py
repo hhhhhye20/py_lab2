cda8a17cd218d98614a506b62187d42d9d622930
hhhhhye20 <tgd_5753@naver.com>
2020-04-26 08:20:24 +0900

    new

--- myp_pkg.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import my_pkg
-
-if __name__=='__main__':
-	num = int(input("Select menu: "))
-	if(num==1):
-		binum=my_pkg.oct(num)	
-		print(binum)	
--- word_p.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys 
-
-if __name__ == '__main__':
-	print(sys.argv[1])
-	word = 'hello'
-	print(word)
